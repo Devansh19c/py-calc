47f70908ca8ccffdbc410431710b045937b179cb
Devansh <devchamoli15997@gmail.com>
2023-02-07 13:42:22 +0530

    Added addition and substraction

--- calc.py
+++ calc.py
@@ -1,1 +1,62 @@
 import math
+
+####################################################################
+def add():
+    list = []
+    sum = 0
+    inp = int()
+    length = int(input("Enter len: "))
+    for i in range(0,length):
+        inp = int(input("Enter number: "))
+        list.append(inp)
+    for i in range(len(list)):
+        sum += list[i]
+    print(f"Sum is :{sum}")
+        
+def subtract():
+    list = []
+    diff = 0
+    inp = int()
+    length = int(input("Enter length: "))
+    for i in range(0,length):
+        inp = int(input("Enter number: "))
+        list.append(inp)
+    for i in range(len(list)):
+        diff -= list[i]
+    print(f"Difference is: {diff}")
+    
+def divide(x,y):
+    return x/y
+def multiply(x,y):
+    return x*y
+def trignometric():
+    pass
+#####################################################################
+
+# print('Below are your following options :')
+# print('1.Addition')
+# print('2.Subtraction')
+# print('3.Division')
+# print('4.Multiply')
+# print('5.Perform a trignometric Calculation')
+choice = int(input("Below are your following options :\n"
+               "1.Addition\n"
+               "2.Subtraction\n"
+               "3.Division\n"
+               "4.Multiply\n"
+               "5.Perform a trignometric Calculation\n"
+               "Your Choice :"))
+
+
+
+match choice:
+    case 1 :
+        add()  
+    case 2 :
+        subtract()
+    case 3 :
+        divide()
+    case 4 :
+        multiply()
+    case 5 :
+        trignometric()
