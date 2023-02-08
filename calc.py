a975aef01ceee51f73f29936d5a4824783b8b76f
Devansh <devchamoli15997@gmail.com>
2023-02-08 13:32:07 +0530

    Creation of a menu into main funct

--- calc.py
+++ calc.py
@@ -1,10 +1,11 @@
 import math
-
+import statistics as st
+# SymPy for calculus, matrices etc
+# MatPlotLibs probably for graph plotting
 ####################################################################
 def add():
     list = []
     sum = 0
-    inp = int()
     length = int(input("Enter len: "))
     for i in range(0,length):
         inp = int(input("Enter number: "))
@@ -12,7 +13,7 @@ def add():
     for i in range(len(list)):
         sum += list[i]
     print(f"Sum is :{sum}")
-        
+    
 def subtract():
     list = []
     diff = 0
@@ -25,8 +26,11 @@ def subtract():
         diff -= list[i]
     print(f"Difference is: {diff}")
     
-def divide(x,y):
-    return x/y
+def divide() -> float:
+    x = int(input('Enter the dividend'))
+    y = int(input('Enter the divisor'))
+    z = x/y
+    print(round(z, 4))
 def multiply(x,y):
     return x*y
 def trignometric():
@@ -39,24 +43,25 @@ def trignometric():
 # print('3.Division')
 # print('4.Multiply')
 # print('5.Perform a trignometric Calculation')
-choice = int(input("Below are your following options :\n"
+def main():
+    choice = int(input("Below are your following options :\n"
                "1.Addition\n"
                "2.Subtraction\n"
                "3.Division\n"
                "4.Multiply\n"
                "5.Perform a trignometric Calculation\n"
                "Your Choice :"))
-
-
-
-match choice:
-    case 1 :
-        add()  
-    case 2 :
-        subtract()
-    case 3 :
-        divide()
-    case 4 :
-        multiply()
-    case 5 :
-        trignometric()
+    match choice:
+        case 1 :
+            add()  
+        case 2 :
+            subtract()
+        case 3 :
+            divide()
+        case 4 :
+            multiply()
+        case 5 :
+            trignometric()
+            
+if __name__ == '__main__':
+    main()
